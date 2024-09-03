d546e3b836e636de183eafa23f1935cf5472c25b
Olga Bursina <bursina.olya@yandex.ru>
2024-09-03 20:37:58 +0500

    Status code 500 test added

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 
 base_url = "https://playground.learnqa.ru/api"
-payload = {"param1": "value1"}
 
-response = requests.post(f"{base_url}/check_type", data=payload)
+response = requests.post(f"{base_url}/get_500")
 print(response.status_code)
 
