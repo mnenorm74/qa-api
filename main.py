850498b64430b324ced113e587a65bc6e97e60a0
Olga Bursina <bursina.olya@yandex.ru>
2024-09-03 20:23:15 +0500

    Response parsing added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 
 base_url = "https://playground.learnqa.ru/api"
-
 payload = {"name": "User"}
-response = requests.get(base_url + "/hello", params=payload)
-print(response.text)
-print(response.status_code)
+
+response = requests.get(f"{base_url}/hello", params=payload)
+parsed_response_text = response.json()
+print(parsed_response_text['answer'])
