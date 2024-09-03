b7e5104c5e79b4057c19e4a59ba11ef0a1713249
Olga Bursina <bursina.olya@yandex.ru>
2024-09-03 20:41:37 +0500

    Status code 301 without redirects added

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 
 base_url = "https://playground.learnqa.ru/api"
 
-response = requests.post(f"{base_url}/something")
+response = requests.post(f"{base_url}/get_301", allow_redirects=False)
 print(response.status_code)
-print(response.text)
 
