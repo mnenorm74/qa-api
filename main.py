aed9bcb0da7bf5f52b63720e2e3dba1cddbdfe33
Olga Bursina <bursina.olya@yandex.ru>
2024-09-03 21:05:37 +0500

    None cookie checking added

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ base_url = "https://playground.learnqa.ru/api"
 payload = {"login": "secret_login", "password": "secret_pass"}
 response1 = requests.post(f"{base_url}/get_auth_cookie", data=payload)
 
-cookie_value = response1.cookies.get('auth_cookie')
-cookies = {'auth_cookie': cookie_value}
+cookie_value = response1.cookies.get('auth_cooki e')
+cookies = {}
+if cookie_value is not None:
+    cookies.update({'auth_cookie': cookie_value})
 
 response2 = requests.post(f"{base_url}/check_auth_cookie", cookies=cookies)
 
