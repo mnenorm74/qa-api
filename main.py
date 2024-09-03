f2e34d18f11d9a2a1dba39eaede29a169f2bdeb9
Olga Bursina <bursina.olya@yandex.ru>
2024-09-03 20:27:53 +0500

    JSON decode handler added

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from json.decoder import JSONDecodeError
 import requests
 
 base_url = "https://playground.learnqa.ru/api"
 payload = {"name": "User"}
 
-response = requests.get(f"{base_url}/hello", params=payload)
-parsed_response_text = response.json()
-print(parsed_response_text['answer'])
+response = requests.get(f"{base_url}/get_text")
+print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Response is not JSON format") 
