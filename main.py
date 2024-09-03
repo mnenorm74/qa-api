dbcd159b21a0f4c994d11d72f2190a4e5b4ee35b
Olga Bursina <bursina.olya@yandex.ru>
2024-09-03 20:52:58 +0500

    Request and response headers added

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ base_url = "https://playground.learnqa.ru/api"
 headers = {"some header": "123"}
 response = requests.get(f"{base_url}/show_all_headers")
 print(response.text)
+print(response.headers)
 
