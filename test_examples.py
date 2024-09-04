7bd9b8cd170f19db1876eec58947631adeceb594
Olga Bursina <bursina.olya@yandex.ru>
2024-09-04 20:09:04 +0500

    Text error added

--- test_examples.py
+++ test_examples.py
@@ -2,9 +2,11 @@ class TestExample:
     def test_check_math(self):
         a = 5
         b = 9
-        assert a + b == 14
+        expected_sum = 14
+        assert a + b == 14,  f"Sum of variables a and b is not equal to {expected_sum}"
 
     def test_check_math2(self):
         a = 5
         b = 11
-        assert a + b == 14
+        expected_sum = 14
+        assert a + b == expected_sum, f"Sum of variables a and b is not equal to {expected_sum}"
