d7f99c8fa7faefe78583e4e2bc148a0d4aa07237
Olga Bursina <bursina.olya@yandex.ru>
2024-09-04 20:37:27 +0500

    File reformatted

--- test_first_api.py
+++ test_first_api.py
@@ -1,17 +1,19 @@
 import requests
+
+
 class TestFirstAPI:
     def test_hello_call(self):
-         base_url = "https://playground.learnqa.ru/api"
-         name = "Olya"
-         data= {'name': name}
+        base_url = "https://playground.learnqa.ru/api"
+        name = "Olya"
+        data = {'name': name}
 
-         response = requests.get(f"{base_url}/hello", params=data)
+        response = requests.get(f"{base_url}/hello", params=data)
 
-         assert response.status_code == 200, "Wrong response code"
+        assert response.status_code == 200, "Wrong response code"
 
-         response_dict = response.json()
-         assert "answer" in response_dict, "There is no field 'answer' in response"
+        response_dict = response.json()
+        assert "answer" in response_dict, "There is no field 'answer' in response"
 
-         expected_response_text = f"Hello, {name}"
-         actual_response_text = response_dict["answer"]
-         assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
+        expected_response_text = f"Hello, {name}"
+        actual_response_text = response_dict["answer"]
+        assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
